dff61f25fa3dfe2cde3306d893dca535d1997b0d
nfatykhova <natalya.fatykhova@mail.ru>
2016-03-03 13:50:17 +0300

    Update Algorithms.java

--- src/ru/levelp/Algorithms.java
+++ src/ru/levelp/Algorithms.java
@@ -33,41 +33,56 @@ public class Algorithms {
        -1, если строка a раньше по алфавиту, чем b
         0, если строки a и b одинаковы
         1, если строка b раньше, чем a
-
        ** Сортировка по всем буквам с учетом длины
         */
 
-        char[] char1 = a.toCharArray(); // преобразуем строки в массив char
-        char[] char2 = b.toCharArray();
+        int minLen = a.length();
+        if (b.length() < minLen )
+            minLen = b.length();
 
-         if (char1[0] < char2[0]) return -1;
-          else if (char1[0] == char2[0]) return 0;
-          else return 1;
-
+        int r = 0;
+        while (r < minLen) {
+            if (a.charAt(r) < b.charAt(r)) return -1;
+            if (a.charAt(r) > b.charAt(r)) return 1;
+            if (a.charAt(r) == b.charAt(r)) r++;
         }
+        if ( a.length() < b.length() ) return -1;
+        if ( a.length() > b.length() ) return 1;
+        else return 0;
+    }
+
 
     public Student[] abcBubbleSort(Student[] students) {
 
         int i;
-        for (i = 0; i < students.length - 1; i++) {
+        for (i = 0; i < students.length; i++) {
             for (int j = 0; j < students.length - 1; j++) {
                 int k = j + 1;
-                int comp = compare(students[j].name, students[k].name);
-                if (comp == 1) {
-                    int temp = j;
-                    k = j;
-                    j = temp;
+                if ((students[j] != null) && (students[k] != null)) {
+                    int comp = compare(students[j].name, students[k].name);
+                    if (comp == 1) {
+                        Student temp = students[j];
+                        students[j] = students[k];
+                        students[k] = temp;
+                    }
                 }
+
             }
         }
-        return
+
+        return students;
+
+    }
+
+    public void printArray(Student[] students) {
         int b = 0;
-        while (students[b] != null) {
+        while ((b < students.length) && (students[b] != null)) {
             students[b].print();
             b++;
         }
 
-
+        }
     }
 
-}
+
+
